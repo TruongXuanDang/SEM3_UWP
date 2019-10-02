5f4d762b6ec837aea9d0a64a30f6e5932c658b0e
TruongDX8 <truonghusk17@gmail.com>
2019-10-02 22:38:25 +0700

    Nav view

--- UWPMidTest/App.xaml.cs
+++ UWPMidTest/App.xaml.cs
@@ -67,7 +67,7 @@ namespace UWPMidTest
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(ListOfSong), e.Arguments);
+                    rootFrame.Navigate(typeof(NavigationViewTwo), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- UWPMidTest/Pages/ShowSongList.xaml.cs
+++ UWPMidTest/Pages/ShowSongList.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
 using System.Threading.Tasks;
+using UWPMidTest.Entities;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -15,10 +15,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using UWPMidTest.Entities;
 using Newtonsoft.Json;
-using System.Collections.ObjectModel;
-using Windows.Data.Json;
+using System.Net.Http;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,35 +25,16 @@ namespace UWPMidTest.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ListOfSong : Page
+    
+    public sealed partial class ShowSongList : Page
     {
-        private const string apiCreateNewSong = "https://2-dot-backup-server-003.appspot.com/_api/v2/songs/post-free";
         private const string apiGetSongList = "https://2-dot-backup-server-003.appspot.com/_api/v2/songs/get-free-songs";
         public ObservableCollection<SongInfo> ListSongs = new ObservableCollection<SongInfo>();
-        public ListOfSong()
+        public ShowSongList()
         {
             this.InitializeComponent();
 
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            var songInfo = new SongInfo
-            {
-                name = Name.Text,
-                description = Description.Text,
-                singer = Singer.Text,
-                author = Author.Text,
-                thumbnail = Thumbnail.Text,
-                link = Link.Text,
-            };
             var httpClient = new HttpClient();
-            HttpContent content = new StringContent(JsonConvert.SerializeObject(songInfo), Encoding.UTF8, "application/json");
-            Task<HttpResponseMessage> httpRequestMessageToCreateNewSong = httpClient.PostAsync(apiCreateNewSong, content);
-            var jsonResultToCreateNewSong = httpRequestMessageToCreateNewSong.Result.Content.ReadAsStringAsync().Result;
-
-            //var listOfSong = new ObservableCollection<SongInfo>();
-            //listOfSong.Add(songInfo);
             Task<HttpResponseMessage> httpRequestMessageToGetSongList = httpClient.GetAsync(apiGetSongList);
             var jsonResultToGetSongList = httpRequestMessageToGetSongList.Result.Content.ReadAsStringAsync().Result;
             ObservableCollection<SongInfo> listSong = JsonConvert.DeserializeObject<ObservableCollection<SongInfo>>(jsonResultToGetSongList);
