66563227e9c3dfa0e3336e139b7c6cfc5b24abbc
TruongDX8 <truonghusk17@gmail.com>
2019-10-04 08:38:17 +0700

    MusicApplication Project: NavigationView

--- MusicApplication/App.xaml.cs
+++ MusicApplication/App.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using MusicApplication.Pages;
+using NavigationView = MusicApplication.Pages.NavigationView;
 
 namespace MusicApplication
 {
@@ -67,7 +68,7 @@ namespace MusicApplication
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(NavView), e.Arguments);
+                    rootFrame.Navigate(typeof(NavigationView), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
