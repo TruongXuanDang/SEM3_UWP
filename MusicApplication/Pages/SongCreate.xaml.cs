d6be4afd5aa54cb28defbbbd157c119c7bd465ec
TruongDX8 <truonghusk17@gmail.com>
2019-10-04 09:57:13 +0700

    MusicApplication: Create Song

--- MusicApplication/Pages/SongCreate.xaml.cs
+++ MusicApplication/Pages/SongCreate.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using MusicApplication.Entities;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +17,48 @@ namespace MusicApplication.Pages
     /// </summary>
     public sealed partial class SongCreate : Page
     {
+        private const string apiCreateNewSong = "https://2-dot-backup-server-003.appspot.com/_api/v2/songs/post-free";
         public SongCreate()
         {
             this.InitializeComponent();
+        }
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var song = new Song
+            {
+                name = Name.Text,
+                description = Description.Text,
+                singer = Singer.Text,
+                author = Author.Text,
+                thumbnail = Thumbnail.Text,
+                link = Link.Text,
+            };
+            if (ValidateInputData(song) == true)
+            {
+                var httpClient = new HttpClient();
+                HttpContent content = new StringContent(JsonConvert.SerializeObject(song), Encoding.UTF8, "application/json");
+                Task<HttpResponseMessage> httpRequestMessageToCreateNewSong = httpClient.PostAsync(apiCreateNewSong, content);
+                var jsonResultToCreateNewSong = httpRequestMessageToCreateNewSong.Result.Content.ReadAsStringAsync().Result;
+                MessageDialog dialog = new MessageDialog("Succeeded");
+                await dialog.ShowAsync();
+            }
+            else
+            {
+                MessageDialog dialog = new MessageDialog("Name, thumbnail, link need to be validated");
+                await dialog.ShowAsync();
+            }
+
+        }
+
+        private bool ValidateInputData(Song songInfo)
+        {
+            if (songInfo.name != "" && songInfo.thumbnail != "" && songInfo.link != "" &&
+                songInfo.name.Length <= 50 && songInfo.link.EndsWith(".mp3"))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
